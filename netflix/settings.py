b1b50543d18dd78d21baea6dc7af3187b313d0d9
muxiddin87 <muhiddin@DESKTOP-RT3JJB4>
2023-01-20 15:40:41 +0500

    settings.py

--- netflix/settings.py
+++ netflix/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
+    BASE_DIR / "staticfiles",
+
 ]
 
 # Default primary key field type
