ee9a2266c053a1c107442f27369b462467af274f
muxiddin87 <muhiddin@DESKTOP-RT3JJB4>
2023-01-20 14:56:37 +0500

    settings.py

--- netflix/urls.py
+++ netflix/urls.py
@@ -17,6 +17,6 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hollywood.urls',)),
-    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-docs'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-docs'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc-docs'),
 ]
