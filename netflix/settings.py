2a924843d4b4a61eaf773bfaf8fa802bd4cc82f1
muxiddin87 <muhiddin@DESKTOP-RT3JJB4>
2023-01-25 14:36:26 +0500

    settings.py

--- netflix/settings.py
+++ netflix/settings.py
@@ -124,7 +124,7 @@ STATICFILES_DIRS = [
 ]"""
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
