cd6faac0db26702afd22785756cffc4b0d090d6b
muxiddin87 <muhiddin@DESKTOP-RT3JJB4>
2023-01-25 16:30:52 +0500

    settings.py

--- netflix/settings.py
+++ netflix/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$3)%wnkp&yw1uszxu4&x+*3$%)vpc569s5bw0w_vdo4xpkxwxm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["moviesapi.muhiddin87.uz"]
 
